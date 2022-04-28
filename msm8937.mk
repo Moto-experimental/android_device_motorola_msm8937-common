7cde8ddf65b8018e4cba51e28d45fd3eb52607fc
GKart <gkartyt@gmail.com>
2022-04-28 16:18:43 +0000

    msm8937-common: build missing ril executables

--- msm8937.mk
+++ msm8937.mk
@@ -311,6 +311,10 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.1.vendor \
+    android.hardware.secure_element@1.2.vendor \
+    android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.2.vendor \
     CarrierConfigOverlay \
     librmnetctl \
